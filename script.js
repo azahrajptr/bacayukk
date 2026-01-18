5658afcafc5e5a1cae5c991f753eb7443fb076cb
azahrajptr <azahraz061@gmail.com>
2026-01-19 01:37:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+//ketika di klik
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+//klik diluar sidebar hilangin nav
+const hamburger = document.querySelector
+('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
